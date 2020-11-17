1f5378efe876e98bee369ffab89fbff3102f0e27
HoLoKoSTeR <nfedoruk2000@gmail.com>
2020-11-17 18:32:04 +0300

    работает со второго раза, НО РАБОТАЕТ зато!1!01

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const { HearManager } = require("@vk-io/hear");
 const fs = require("fs");
 const delay = require("delay");
 const youtubedl = require("youtube-dl");
-var us="7e356f4764ad607c1e54ccde9ae788eff089aa695474b06d765a90f71d0b0b934330b2861e904381165bc";
-var gr="8284a9cac00ca36cdea296718d241fbf32000b0c9df12bdf643eaab8401bbcc9aaf57d0c719bf499cd673";
-var ngr=171430987;
+var us =
+  "7e356f4764ad607c1e54ccde9ae788eff089aa695474b06d765a90f71d0b0b934330b2861e904381165bc";
+var gr =
+  "8284a9cac00ca36cdea296718d241fbf32000b0c9df12bdf643eaab8401bbcc9aaf57d0c719bf499cd673";
+var ngr = 171430987;
 const group = new VK({
-  token:
-    gr,
+  token: gr,
   pollingGroupId: ngr,
 });
 const user = new VK({
-  token:
-    us,
+  token: us,
 });
 group.updates.on("message", (context, next) => {
   if (context.isOutbox) {
@@ -21,7 +21,7 @@ group.updates.on("message", (context, next) => {
   }
 
   return next();
-});8
+});
 const hearManager = new HearManager();
 group.updates.on("message_new", hearManager.middleware);
 hearManager.hear(/yt/i, async (context) => {
@@ -74,7 +74,7 @@ hearManager.hear(/yt/i, async (context) => {
             },
           },
           title: name,
-          group_id:ngr,
+          group_id: ngr,
         })
         .then((doc) => {
           console.log("Success upload", doc);
@@ -136,7 +136,7 @@ hearManager.hear(/yt/i, async (context) => {
           name: name,
           description: dsc,
           is_private: 1,
-          group_id:ngr,
+          group_id: ngr,
         })
         .then((vid) => {
           console.log("Success upload", vid);
@@ -148,6 +148,82 @@ hearManager.hear(/yt/i, async (context) => {
             attachment: `video${vid.ownerId}_${vid.id}`,
           });
           fs.unlinkSync(sus);
+        })
+        .catch((err) => {
+          console.error(err);
+          context.send(`Произошла ошибка: ${err}`);
+        });
+    }
+  }
+  if (args[2].toLowerCase() == "music") {
+    await yt((val, url) => {
+      end((end) => {
+        if (end == "end")
+          rename_mp3(val).then((fl) => {
+            upload_music(fl, url);
+          });
+      });
+    });
+    console.log("=====================================");
+    async function yt(callback) {
+      return video.on("info", (info) => {
+        context.send("Начинаю загрузку");
+        console.log("Начинаю загрузку");
+        console.log(`size: ${info.size / 1048576} Мб`);
+        console.log(`НАзвание: ${info._filename}`);
+        if (info.size < 2147483600) {
+          video.pipe(fs.createWriteStream("tmp/" + info._filename));
+        } else {
+          context.send("Размер файла больше 2ГБ, увы(");
+        }
+        var filename = info._filename.split(".")[0];
+        return callback(filename, info._filename, info.ext);
+      });
+    }
+
+    async function end(callback) {
+      return video.on("end", () => {
+        console.log("Закончил загрузку");
+        return callback("end");
+      });
+    }
+    async function rename_mp3(file) {
+      var dir = __dirname + "\\tmp\\";
+      fs.rename(`${dir + file}.mp3`, `${dir + file}.mp3.mus`, (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("\nFile Renamed\n");
+        }
+      });
+      return `${file}.mp3.mus`;
+    }
+    async function upload_music(name, u) {
+      sus = __dirname + `\\tmp\\${u}`;
+      noext = __dirname + `\\tmp\\${name}`;
+      await user.upload
+        .document({
+          source: {
+            timeout: 1e3 * 60,
+            values: {
+              value: fs.readFileSync(noext), //u,
+              filename: noext,
+            },
+          },
+          title: u,
+          group_id: ngr,
+        })
+        .then((doc) => {
+          console.log("Success upload", doc);
+          //   console.log(context)
+          group.api.messages.edit({
+            peer_id: context.peerId,
+            message_id: context.id + 1,
+            message: "Документ загружен!",
+            attachment: `doc${doc.ownerId}_${doc.id}`,
+          });
+         // fs.unlinkSync(sus);
+          fs.unlinkSync(noext);
         })
         .catch((err) => {
           console.error(err);
